5931885908921951944ee0ddf82eca10b7e6f105
omar <omarnabilsharnopu@gmail.cm>
2024-07-15 18:54:43 +0300

    init commit

---
+++ lib/core/error/error_model.dart
@@ -0,0 +1,13 @@
+class ErrorModel {
+  final int status;
+  final String errorMessage;
+
+  ErrorModel({required this.status, required this.errorMessage});
+
+  factory ErrorModel.fromJson(Map jsonData) {
+    return ErrorModel(
+      status: jsonData['status'],
+      errorMessage: jsonData['ErrorMessage'],
+    );
+  }
+}
---
+++ lib/core/utils/app_strings.dart
@@ -0,0 +1,4 @@
+class AppStrings {
+  static const String routeApp = 'Route App';
+  static const String hintSearch = 'what do you search for?';
+}
